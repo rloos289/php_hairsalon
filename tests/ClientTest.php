972a1fce319c3cfe2ee78bd0437f602cdcdb71fa
Ryan Loos <RLoos289@gmail.com>
2016-09-23 11:06:50 -0700

    add save, delete, getall, deleteall, and update tests to client class

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -28,7 +28,90 @@
     //then run phpunit tests
 
     {
+        protected function teardown()
+        {
+            Stylist::deleteAll();
+            Client::deleteAll();
+        }
 
-    }
+        function test_save()
+        {
+            $client = "Lauren";
+            $test_client = new Client($client);
+            $test_client->save();
+
+            $result = Client::getAll();
+
+            $this->assertEquals($test_client, $result[0]);
+        }
+
+        function test_get_all()
+        {
+            $client1 = "Lauren";
+            $client2 = "Mitch";
+            $test_client1 = new Client($client1);
+            $test_client1->save();
+            $test_client2 = new Client($client2);
+            $test_client2->save();
+
+            $result = Client::getAll();
+
+            $this->assertEquals([$test_client1, $test_client2], $result);
+        }
+
+        function test_delete_all()
+        {
+            $client1 = "Lauren";
+            $client2 = "Mitch";
+            $test_client1 = new Client($client1);
+            $test_client1->save();
+            $test_client2 = new Client($client2);
+            $test_client2->save();
+
+            Client::deleteAll();
+            $result = Client::getAll();
+
+            $this->assertEquals([], $result);
+        }
+
+        function test_find_client()
+        {
+            $client1 = "Lauren";
+            $client2 = "Mitch";
+            $test_client1 = new Client($client1);
+            $test_client1->save();
+            $test_client2 = new Client($client2);
+            $test_client2->save();
+
+            $result = Client::find($test_client1->getId());
+
+            $this->assertEquals($test_client1, $result);
+        }
+
+        function test_delete_client()
+        {
+            $client = "Lauren";
+            $test_client = new Client($client);
+            $test_client->save();
+            $id = $test_client->getId();
+            $test_client->delete();
+
+            $result = Client::find($id);
+
+            $this->assertEquals(null, $result);
+        }
+
+        function test_update()
+        {
+            $client = "Lauren";
+            $test_client = new Client($client);
+            $test_client->save();
+            $test_client->update("Lory");
+
+            $result = $test_client->getName();
+
+            $this->assertEquals("Lory", $result);
+        }
+   }
 
  ?>
